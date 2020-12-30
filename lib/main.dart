aed6fe65b299c8515437a3c3d2ee8e2c8a6a4dd2
Sri Vignesh <srivignesh1406@gmail.com>
2020-12-30 20:08:27 +0530

    locked the orientation of the app to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,6 @@ import 'package:new_tic_tac_toe/AutoGamePage.dart';
 import 'GamePage.dart';
 import 'Homepage.dart';
 
-//
-// void main() {
-//   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-//       .then((_) {
-//     runApp(MaterialApp(
-//       // initialRoute: '/dummy',
-//       routes: {
-//         '/': (context) => HomePage(),
-//         '/autoGame': (context) => AutoGamePage(),
-//         '/game': (context) => GamePage(),
-//         // '/dummy': (context) => MyHome()
-//       },
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(primarySwatch: Colors.grey),
-//     ));
-//   });
-// }
-
 void main() {
   runApp(MyApp());
 }
